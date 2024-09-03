53ba9d0f6708183eda495ebd1cf57f61a26a88b8
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-09-03 13:12:52 +0300

    clerk authentication is setup - mobile navbar created - Toast message added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-      Navbar
-      {children}
-      Footer
+      <StreamVideoProvider>{children}</StreamVideoProvider>
     </main>
   );
 };
