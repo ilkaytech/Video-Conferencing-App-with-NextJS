5fea5f819130f27158308a94fe23792a86ae095c
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-09-03 19:16:29 +0300

    meeting room created

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
